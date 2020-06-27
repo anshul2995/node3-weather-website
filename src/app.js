8a293a6ab3a4b8b7323f5096d857cb4df38d4e55
VMittAn <anshul.mittal@pearson.com>
2020-06-28 02:35:22 +0530

    added about text

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Anshul Mittal'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Anshul Mittal'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Anshul Mittal'
     })
 })
 
@@ -71,7 +71,7 @@ app.get('/weather', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Anshul Mittal',
         errorMessage: 'Help article not found.'
     })
 })
@@ -79,7 +79,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Anshul Mittal',
         errorMessage: 'Page not found.'
     })
 })
